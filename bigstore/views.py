2cba195c352475145f2e0f5c97de0f595a88652b
Mahbubur Rahman <mahabub.rahman708@gmail.com>
2019-09-09 20:09:31 +0600

    index template creating and mastering

--- bigstore/views.py
+++ bigstore/views.py
@@ -1,5 +1,5 @@
-from django.http import HttpResponse
+from django.shortcuts import render
 
 
 def index(request):
-    return HttpResponse("<h1>Hello from supershop index page</h1>")
+    return render(request, 'master.html')
